3326e3063de7f56c0046f5166af7f138d8838550
DESKTOP-SSRNIT7\Teeno <o.pereira>
2026-06-09 01:13:01 +0100

    Disparos

--- src/main.js
+++ src/main.js
@@ -17,6 +17,8 @@ const config = {
 };
 
 const PLAYER_SPEED = 220;
+const BULLET_SPEED = 420;
+const FIRE_RATE = 250;
 
 const game = new Phaser.Game(config);
 
@@ -25,11 +27,16 @@ function preload() {
 
 function create() {
   createPlayerTexture(this);
+  createBulletTexture(this);
 
   this.player = this.physics.add.sprite(400, 300, "player");
   this.player.setCollideWorldBounds(true);
 
+  this.bullets = this.physics.add.group();
+  this.nextShotTime = 0;
+
   this.cursors = this.input.keyboard.createCursorKeys();
+  this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
   this.wasd = this.input.keyboard.addKeys({
     up: Phaser.Input.Keyboard.KeyCodes.W,
     left: Phaser.Input.Keyboard.KeyCodes.A,
@@ -37,17 +44,31 @@ function create() {
     right: Phaser.Input.Keyboard.KeyCodes.D
   });
 
-  this.add.text(16, 16, "Mover: WASD ou setas", {
+  this.add.text(16, 16, "Mover: WASD/setas | Disparar: SPACE", {
     fontSize: "18px",
     color: "#ffffff"
   });
 }
 
 function update() {
-  const left = this.cursors.left.isDown || this.wasd.left.isDown;
-  const right = this.cursors.right.isDown || this.wasd.right.isDown;
-  const up = this.cursors.up.isDown || this.wasd.up.isDown;
-  const down = this.cursors.down.isDown || this.wasd.down.isDown;
+  handlePlayerMovement(this);
+
+  if (this.spaceKey.isDown) {
+    const angle = this.player.rotation - Math.PI / 2;
+    const targetX = this.player.x + Math.cos(angle) * 100;
+    const targetY = this.player.y + Math.sin(angle) * 100;
+
+    shootBullet(this, targetX, targetY);
+  }
+
+  removeOffscreenBullets(this);
+}
+
+function handlePlayerMovement(scene) {
+  const left = scene.cursors.left.isDown || scene.wasd.left.isDown;
+  const right = scene.cursors.right.isDown || scene.wasd.right.isDown;
+  const up = scene.cursors.up.isDown || scene.wasd.up.isDown;
+  const down = scene.cursors.down.isDown || scene.wasd.down.isDown;
 
   const velocity = new Phaser.Math.Vector2(0, 0);
 
@@ -64,11 +85,47 @@ function update() {
   }
 
   velocity.normalize().scale(PLAYER_SPEED);
-  this.player.setVelocity(velocity.x, velocity.y);
+  scene.player.setVelocity(velocity.x, velocity.y);
 
   if (velocity.lengthSq() > 0) {
-    this.player.setRotation(velocity.angle() + Math.PI / 2);
+    scene.player.setRotation(velocity.angle() + Math.PI / 2);
   }
+}
+
+function shootBullet(scene, targetX, targetY) {
+  if (scene.time.now < scene.nextShotTime) {
+    return;
+  }
+
+  const direction = new Phaser.Math.Vector2(
+    targetX - scene.player.x,
+    targetY - scene.player.y
+  );
+
+  if (direction.lengthSq() === 0) {
+    return;
+  }
+
+  direction.normalize();
+
+  const bullet = scene.bullets.create(scene.player.x, scene.player.y, "bullet");
+  bullet.setVelocity(direction.x * BULLET_SPEED, direction.y * BULLET_SPEED);
+  bullet.setRotation(direction.angle() + Math.PI / 2);
+
+  scene.nextShotTime = scene.time.now + FIRE_RATE;
+}
+
+function removeOffscreenBullets(scene) {
+  scene.bullets.children.each((bullet) => {
+    if (
+      bullet.x < -20 ||
+      bullet.x > scene.scale.width + 20 ||
+      bullet.y < -20 ||
+      bullet.y > scene.scale.height + 20
+    ) {
+      bullet.destroy();
+    }
+  });
 }
 
 function createPlayerTexture(scene) {
@@ -79,5 +136,14 @@ function createPlayerTexture(scene) {
   graphics.lineStyle(3, 0xffffff, 1);
   graphics.strokeTriangle(16, 0, 32, 32, 0, 32);
   graphics.generateTexture("player", 32, 32);
+  graphics.destroy();
+}
+
+function createBulletTexture(scene) {
+  const graphics = scene.add.graphics();
+
+  graphics.fillStyle(0xf2cc60, 1);
+  graphics.fillCircle(6, 6, 6);
+  graphics.generateTexture("bullet", 12, 12);
   graphics.destroy();
 }
